6081d7a464ec31d4cf13736593c098d05cf5a54a
pfzoz <pedrozoz.sizaan@gmail.com>
2023-06-17 23:28:38 -0300

    first-pancake

--- golang/pancakeSort/main.go
+++ golang/pancakeSort/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // pega máximo
 // flipa começo até máximo
 // flipa tudo até current
@@ -8,37 +10,48 @@ package main
 
 func array_max(array []int) int {
 	max := array[0]
-	for i := 1; i < len(array); i++ {
+	i := 1
+	for i = 1; i < len(array); i++ {
 		if array[i] > max {
 			max = array[i]
 		}
 	}
-	return max
+	fmt.Println(max)
+	return i
 }
 
-func flip_array(array []int, index int) {
+func flip_array(array *[]int, index int) {
 	temp := 0
 	start := 0
 	for start < index {
-		temp = array[start]
-		array[start] = array[index]
-		array[index] = temp
+		temp = (*array)[start]
+		(*array)[start] = (*array)[index]
+		(*array)[index] = temp
 		start += 1
 		index -= 1
 	}
+	fmt.Println(array)
 }
 
-func pancake_sort(array []int) {
+func pancake_sort(array *[]int) {
 
-	for current_size := len(array) - 1; current_size > 1; current_size -= 1 {
-		max := array_max(array[:current_size])
-		if max != current_size-1 {
-			flip_array(array, max)
-			flip_array(array, current_size-1)
+	n := len((*array))
+	for i := n - 1; i != -1; i-- {
+		max_ := i
+		for j := i; j != -1; j-- {
+			if (*array)[j] > (*array)[max_] {
+				max_ = j
+			}
+		}
+		if max_ != i {
+			flip_array(array, max_)
+			flip_array(array, i)
 		}
 	}
 }
 
 func main() {
-
+	myArray := []int{4, 2, 3, 5, 10, 2, 1, 10, 2, 2}
+	pancake_sort(&myArray)
+	fmt.Println(myArray)
 }
